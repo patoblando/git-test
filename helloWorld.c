a6da163848c4edfcac4655a3b811e7547371001e
patoblando <85848059+patoblando@users.noreply.github.com>
2022-05-03 18:39:21 -0300

    Cambiado main
    Ahora el programa recibe argumentos. Si recibe helloWorld no se que hará... Si recibe otra cosa dice que el comando es invalido.

--- helloWorld.c
+++ helloWorld.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void helloWorld(){
 	printf("Hello world!\n");
 }
 
 int main (int argc,char* args[]){
-	helloWorld();
+	if(!strcmp(args[1],"helloWorld"))
+		helloWorld();
+	else
+		printf("Comando invalido. El unico comando disponible  es helloWorld");
 	return 0;
 }
